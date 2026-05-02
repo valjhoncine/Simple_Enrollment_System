534533b31ab15ab539ef0850012db24484edfed5
Jhoncine Valderrama (valjhoncine) <jcval0817@gmail.com>
2026-05-02 08:14:52 +0800

    added datatable cdn

--- includes/partials/head.php
+++ includes/partials/head.php
@@ -8,3 +8,4 @@
 <link href="<?= BASE_URL . '/assets/sb-admin/css/styles.css' ?>" rel="stylesheet" />
 <script data-search-pseudo-elements defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/js/all.min.js" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.29.0/feather.min.js" crossorigin="anonymous"></script>
+<link href="https://cdn.datatables.net/v/dt/jq-3.7.0/dt-2.3.8/datatables.min.css" rel="stylesheet" integrity="sha384-pq10ZYwmet1tp386fLkrSm/N7RUYsWsdYI3cFFJxvfY91mTNBl96Fw3/u4pYU3l2" crossorigin="anonymous">
--- includes/partials/scripts.php
+++ includes/partials/scripts.php
@@ -1,3 +1,4 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="https://cdn.datatables.net/v/dt/jq-3.7.0/dt-2.3.8/datatables.min.js" integrity="sha384-6GudBuzC533FClgQWy/gDB8pVYcyEr0WN8QDbQu+kNaxeGolpBLCdhBMvrHr7gF8" crossorigin="anonymous"></script>
 <script src="<?= BASE_URL . '/assets/sb-admin/js/scripts.js' ?>"></script>
